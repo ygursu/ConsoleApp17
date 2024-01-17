a931b2dbdf1d2d75a95c58f69fe02de718b5922e
ygursu <yasemingrs@gmail.com>
2024-01-17 22:44:23 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,24 @@
+﻿private void btnCalculate_Click(object sender, EventArgs e)
+{
+    DateTime startDate = dateTimePickerStartDate.Value;
+    DateTime endDate = dateTimePickerEndDate.Value;
+
+    int years = endDate.Year - startDate.Year;
+    int months = endDate.Month - startDate.Month;
+    int days = endDate.Day - startDate.Day;
+
+    if (days < 0)
+    {
+        months--;
+        days += DateTime.DaysInMonth(startDate.Year, startDate.Month);
+    }
+
+    if (months < 0)
+    {
+        years--;
+        months += 12;
+    }
+
+    MessageBox.Show(String.Format("Fark: {0} yıl, {1} ay, {2} gün", years, months, days));
+}
+
